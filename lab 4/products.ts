894bb3169c5dae115e25b91286cb9a6db02c5cf6
Inara <98634691+a-inara-b@users.noreply.github.com>
2023-02-22 21:47:39 +0600

    Add files via upload

---
+++ lab 4/products.ts
@@ -0,0 +1,109 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  link: string;
+  rating: number;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Смартфон Apple iPhone 13 128Gb черный',
+    price: 373090,
+    description:
+      'Технология NFC: да. Цвет: черный',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h01/h10/46392662491166/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'Смартфон Apple iPhone 11 128Gb Slim Box черный',
+    price: 292758,
+    description:
+      'технология NFC: да. Цвет: черный',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h0a/h2e/31489167622174/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: 'Смартфон Apple iPhone 14 Pro Max 256Gb фиолетовый',
+    price: 694555,
+    description: 'Технология NFC: Да. Цвет: фиолетовый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h61/62948780736542/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    rating: 4.9,
+  },
+  {
+    id: 4,
+    name: 'Смартфон Apple iPhone 13 128Gb белый',
+    price: 371290,
+    description: 'Технология NFC: да. Цвет: белый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h05/h2c/46392664195102/apple-iphone-13-128gb-belyj-102298420-1-Container.jpg',
+    rating: 4.7,
+  },
+  {
+    id: 5,
+    name: 'Смартфон Apple iPhone 14 128Gb голубой',
+    price: 420770,
+    description: 'Технология NFC: да. Цвет: голубой',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha0/hb3/63073057144862/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
+    rating: 5,
+  },
+  {
+    id: 6,
+    name: 'Смартфон Apple iPhone 13 128Gb розовый',
+    price: 372770,
+    description: 'Технология NFC: да. Цвет: розовый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-rozovyi-102298145/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h93/h8a/46392660721694/apple-iphone-13-128gb-rozovyj-102298145-1-Container.jpg',
+    rating: 4.5,
+  },
+  {
+    id: 7,
+    name: 'Смартфон Apple iPhone 13 128Gb зеленый',
+    price: 392770,
+    description: 'Технология NFC: да. Цвет: зеленый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-zelenyi-104078887/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h9d/hf8/49319164575774/apple-iphone-13-128gb-zelenyj-104078887-1.jpg',
+    rating: 4.9,
+  },
+  {
+    id: 8,
+    name: 'Смартфон Apple iPhone 14 Pro Max 256Gb золотистый',
+    price: 695370,
+    description: 'Технология NFC: да. Цвет: золотистый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-zolotistyi-106363335/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h41/h74/63073980284958/apple-iphone-14-pro-max-128gb-zolotistyj-106363335-1.jpg',
+    rating: 4.85,
+  },
+  {
+    id: 9,
+    name: 'Смартфон Apple iPhone 14 256Gb сиреневый',
+    price: 495880,
+    description: 'Технология NFC: да. Цвет: сиреневый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-256gb-sirenevyi-106363141/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd0/hb7/63072546914334/apple-iphone-14-128gb-sirenevyj-106363141-1.jpg',
+    rating: 4.73,
+  },
+  {
+    id: 10,
+    name: 'Смартфон Apple iPhone 14 Pro 128Gb серебристый',
+    price: 590970,
+    description: 'Технология NFC: да. Цвет: серебристый',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-128gb-serebristyi-106363274/?c=750000000#!/item',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4c/hff/63073746190366/apple-iphone-14-pro-128gb-belyj-106363274-1.jpg',
+    rating: 4.1,
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
